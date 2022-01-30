2a4f6941f4b625dd309394ee4ed8e9aaacb386a2
André Ciornavei <andre.ciornavei@gmail.com>
2022-01-30 17:50:13 -0300

    implemented p2p server

---
+++ src/server_p2p.ts
@@ -0,0 +1,66 @@
+/*********************************************************/
+/** Import websocket lib to enable p2p solution         **/
+/*********************************************************/
+import Websocket from "ws"
+
+/*********************************************************/
+/** Retrieve the peer port to try connection            **/
+/*********************************************************/
+const P2P_PORT = (process.env.P2P_PORT || 5001) as number
+
+/*********************************************************/
+/** Retrieve all peers the server will try to connect   **/
+/*********************************************************/
+const peers = process.env.PEERS ? process.env.PEERS.split(",") : []
+
+
+/*********************************************************/
+/** Define the class responsible to manage peers        **/
+/*********************************************************/
+export class P2PServer {
+
+    sockets: Websocket[]
+
+    constructor(blockchain) {
+        this.blockchain = blockchain
+        this.sockets = []
+    }
+
+    listen() {
+        const server = new Websocket.Server({ port: P2P_PORT })
+        server.on("connection", socket => this.connectSocket(socket))
+        this.connectToPeers()
+        console.log(`Listening for p2p connections on ${P2P_PORT}`)
+    }
+
+    connectToPeers() {
+        peers.forEach(peer => {
+            const socket = new Websocket(peer)
+            socket.on('open', () => this.connectSocket(socket))
+        })
+    }
+
+    connectSocket(socket) {
+        this.sockets.push(socket)
+        console.log(`Socket connected`)
+        this.messageHandler(socket)
+        this.sendChain(socket)
+    }
+
+    messageHandler(socket: Websocket) {
+        socket.on('message', (message: Websocket.RawData) => {
+            const data = JSON.parse(message.toString())
+            this.blockchain.replaceChain(data)
+        })
+    }
+
+    sendChain(socket: Websocket) {
+        socket.send(JSON.stringify(this.blockchain.chain))
+    }
+
+    syncChains() {
+        for (const socket of this.sockets) {
+            this.sendChain(socket)
+        }
+    }
+}
